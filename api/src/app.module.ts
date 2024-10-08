83545580d00eb548b23ae559f92ed2785be7f843
Paco Morcillo Vizuete <pacomorcillovizuete@gmail.com>
2024-10-08 22:32:52 +0200

    Add ServeStaticModule to allow to serve static content

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,15 +3,20 @@ import { PokemonsModule } from './pokemons/pokemons.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
-    PokemonsModule,
-    AuthModule,
-    UsersModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PokemonsModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
